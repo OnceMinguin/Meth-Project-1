332c57d73b0c444ca5be6966f749a071fc653c07
User <theshien123@gmail.com>
2021-09-27 16:35:45 -0400

    adwafwaf

--- src/adafnwa/Album.java
+++ src/adafnwa/Album.java
@@ -1,6 +1,10 @@
-//@author Andy Li Henry Lin
 package adafnwa;
 
+/**
+ * Stores all the parameters of an Album.
+ * Compares if two albums are the same.
+ * @author Andy Li, Henry Lin
+ */
 public class Album {
     private String title;
     private String artist;
@@ -8,6 +12,14 @@ public class Album {
     private Date releaseDate;
     private boolean isAvailable;
 
+    /**
+     * Constructor for an album object
+     * @param title of the album
+     * @param artist creating the album
+     * @param genre of the ablbum represented by Genre object
+     * @param releaseDate of the album represented by a Date object
+     * @param isAvailable of the album representing if the album is being loaned out
+     */
     public Album(String title, String artist, Genre genre, Date releaseDate, boolean isAvailable){
         this.title = title;
         this.artist = artist;
@@ -16,10 +28,24 @@ public class Album {
         this.isAvailable = isAvailable;
     }
 
+    /**
+     * Compares title and album parameters of two albums
+     * @param obj that is the album to be compared to
+     * @return true if the two albums are the same and false otherwise
+     */
     @Override
-    public boolean equals(Album obj) {
-        return this.title.equals(obj.title) && this.artist.equals(obj.artist);
+    public boolean equals(Object obj) {
+        if(obj instanceof Album){
+            Album comp = (Album) obj;
+            return this.title.equals(comp.title) && this.artist.equals(comp.artist);
+        }
+       return false;
     }
+
+    /**
+     * Converts an album to string
+     * @return the string form of the album
+     */
     @Override
     public String toString() {
         String album = this.title + "::" + this.artist + "::" + genre.toString(genre)
@@ -33,10 +59,18 @@ public class Album {
         return album;
     }
 
-    public String titleToString() {
+    /**
+     * converts title and artist to a string
+     * @return string form of title and artist
+     */
+    public String titleToString(){
         return this.title + "::" + this.artist;
     }
 
+    /**
+     * loans out the book and changes status to available
+     * @return true if book is loanable and false if book is already lent out.
+     */
     public boolean loanedOut(){
         if (this.isAvailable){
             this.isAvailable = false;
@@ -44,6 +78,11 @@ public class Album {
         }
         return false;
     }
+
+    /**
+     * returns the book and changes status to available
+     * @return true if book is returnable and false if book is not able to be returned
+     */
     public boolean returnAlbum(){
         if (!this.isAvailable){
             this.isAvailable = true;
@@ -51,9 +90,19 @@ public class Album {
         }
         return false;
     }
+
+    /**
+     * converts the genre class of an object to String
+     * @return string form of the genre
+     */
     public String getGenre(){
         return this.genre.toString();
     }
+
+    /**
+     * gets the date
+     * @return the release date
+     */
     public Date getDate(){
         return this.releaseDate;
     }
--- src/adafnwa/RunProject1.java
+++ src/adafnwa/RunProject1.java
@@ -1,7 +1,14 @@
-//@author Andy Li Henry Lin
 package adafnwa;
 
+/**
+ * Driver class to run collection manager
+ * @author Andy Li, Henry Lin
+ */
 public class RunProject1 {
+    /**
+     * runs collection manager
+     * @param args
+     */
     public static void main(String[] args) {
         new CollectionManager().run();
     }
